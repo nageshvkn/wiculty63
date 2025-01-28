f50472b0aaab9f7e9ab4e6a8c388b005cb7931ab
Nageswara Rao P <nageswara.rao@wiculty.com>
2025-01-28 20:36:27 +0530

    fixed bug 123

--- LoginUser.java
+++ LoginUser.java
@@ -1,5 +1,11 @@
 public class Login
 {
-	public static void main()
+	public static void main(String args[]) {
+		int i;
+
+		for(i=0;i<=10;i++) {
+			System.out.println("Number: " + i);
+		}
+	}
 
 }
